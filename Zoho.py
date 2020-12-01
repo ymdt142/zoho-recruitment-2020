b957a244c2cc0ea2957d39370465c69373e06b06
ymdt142 <31586415+ymdt142@users.noreply.github.com>
2020-12-01 11:02:22 +0530

    Update Zoho.py

--- Zoho.py
+++ Zoho.py
@@ -1,14 +1,16 @@
-n=5
+n=9
 i=0
 j=0
 k=1
 while(i<n):
     j=0
     while(j<n):
-        if(i+j==n-1):
+        a=i + j
+        if(a==n-1):
             for ii in range(i+1):
-                print(str(k),end="")
+                print(k,end="")
                 k+=1
+                j+=1
         else:
             print(" ",end="")
         j+=1
